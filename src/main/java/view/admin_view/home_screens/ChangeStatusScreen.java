524d9199be257c7f73a442b720ab3ae9dfc330b2
khuong1742005 <boyzsno1@gmail.com>
2025-02-18 23:06:24 +0700

    #130 - fea: repaint UI

--- src/main/java/view/admin_view/home_screens/ChangeStatusScreen.java
+++ src/main/java/view/admin_view/home_screens/ChangeStatusScreen.java
@@ -23,8 +23,6 @@ public class ChangeStatusScreen extends JFrame{
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
 
@@ -32,30 +30,26 @@ public class ChangeStatusScreen extends JFrame{
         setUndecorated(true);
 
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 10, 340, 200));
         jPanel1.setBackground(colorFix);
 
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setIcon(new javax.swing.ImageIcon(basePath + "\\src\\main\\java\\assets\\output-onlinegiftools.gif"));
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 10, 340, 200));
 
         jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 18));
-        jLabel2.setText("Bạn muốn ban?");
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 220, 150, 30));
-
-        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 14));
-        jLabel3.setText("Thời gian (phút):");
-        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 280, 120, 30));
-        jPanel1.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 280, 290, 30));
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("Bạn muốn ban?     ");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 230, 210, 30));
 
         jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel4.setText("Xác nhận");
         jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 16));
-        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 330, 170, 80));
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 280, 170, 80));
 
         jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel5.setText("Hủy");
         jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 16));
-        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 320, 170, 90));
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 280, 170, 90));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -73,11 +67,9 @@ public class ChangeStatusScreen extends JFrame{
 
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JTextField jTextField1;
 
     public JLabel getjLabel4() {
         return jLabel4;
@@ -87,7 +79,4 @@ public class ChangeStatusScreen extends JFrame{
         return jLabel5;
     }
 
-    public JTextField getjTextField1() {
-        return jTextField1;
-    }
 }
